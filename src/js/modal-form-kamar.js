26add0b4664588eccb4abd5d6272f6334b9cc6be
bahe <bahebdg@gmail.com>
2023-08-22 20:58:25 +0700

    mulai mengerjakan detail dan edit

--- src/js/modal-form-kamar.js
+++ src/js/modal-form-kamar.js
@@ -1,8 +1,30 @@
-const modal = document.getElementById('modal');
+// form
+const modal = document.getElementById('modal-form');
 const header = document.querySelector('header');
 const container = document.getElementById('container');
-
 const form = document.getElementById('form-tambah');
 form.addEventListener('click', function(){
     modal.classList.toggle('hidden');
-})
+});
+
+// detail
+const details = document.querySelectorAll('.ri-search-line');
+const cek_detail = function(e){
+    e.addEventListener('click', ()=>{
+        console.log('klik detail')
+    })
+}
+for (const detail of details){
+    cek_detail(detail);
+}
+
+// edit
+const edits = document.querySelectorAll('.ri-file-edit-line');
+const cek_edit = function(e){
+    e.addEventListener('click', ()=>{
+        console.log('klik edit')
+    })
+}
+for (const edit of edits){
+    cek_edit(edit);
+}
