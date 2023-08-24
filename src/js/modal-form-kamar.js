4875ecfa0f24760d18a2b078773ae2a3b6ce4251
bahe <bahebdg@gmail.com>
2023-08-24 18:03:07 +0700

    menjadikan form lebih baik dari sizing

--- src/js/modal-form-kamar.js
+++ src/js/modal-form-kamar.js
@@ -5,14 +5,16 @@ const layer = document.getElementById('layer');
 const utama = document.querySelector('main');
 const layer_tabel = document.querySelector('#modal-form div')
 
+const layer_h = 'h-[calc(100vh+70px)]';
+
 //form
 const modal_form = document.getElementById('modal-form');
 const form = document.getElementById('form-tambah');
 form.addEventListener('click', function(){
     modal_form.classList.toggle('hidden');
     layer.classList.toggle('hidden');
-    layer.classList.toggle('h-[calc(100vh+60px)]');
-    layer_tabel.classList.toggle('h-[calc(100vh+60px)]');
+    layer.classList.toggle(layer_h);
+    layer_tabel.classList.toggle(layer_h);
     header.classList.toggle('grayscale');
     container.classList.toggle('grayscale');
     utama.classList.toggle('h-screen');
