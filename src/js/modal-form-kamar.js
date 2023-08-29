75b65b0928280c13a8dab984b2ac6f579b65f357
bahe <bahebdg@gmail.com>
2023-08-29 07:39:31 +0700

    belajar scrip

--- src/js/modal-form-kamar.js
+++ src/js/modal-form-kamar.js
@@ -2,19 +2,37 @@
 const header = document.querySelector('header');
 const container = document.getElementById('container');
 const layer = document.getElementById('layer');
+const layer_h = 'h-screen';
+const bg = 'grayscale';
 
-// class table data
+function display(){
+    layer.classList.toggle('hidden');
+    layer.classList.add(layer_h);
+    header.classList.add(bg);
+    container.classList.add(bg);
+}
+function modal(form){
+    form.classList.toggle('hidden');
+    form.classList.add('flex');
+    form.classList.add(layer_h);
+}
+
+// table data
 const tds = document.querySelectorAll('td');
 for(const td of tds){
     td.setAttribute('class','p-4 border-secondary-gray border border-b-2 border-opacity-10');
     td.classList.remove('hidden');
 }
-// class table header
-// const ths = document.querySelectorAll('th');
-// for(const th of ths){
-//     th.setAttribute('class','border border-b-2 border-opacity-10 border-secondary-blue p-4 text-left')
-// }
 
+//-------- Add
+const modal_add = document.getElementById('modal-add');
+const tombol = document.getElementById('tambah');
+tombol.addEventListener('click', ()=>{
+    display();
+    modal(modal_add);
+});
+
+// ------detail section
 // detail label
 const detail_labels = document.querySelectorAll('#detail label');
 for(const label of detail_labels){
@@ -29,75 +47,41 @@ const detail_spans = document.querySelectorAll('#detail span');
 for(const span of detail_spans){
     span.classList.add('text-lg');
 }
-
-const layer_h = 'h-screen';
-const bg = 'grayscale';
-
-//form
-const modal_form = document.getElementById('modal-form');
-const form = document.getElementById('form-tambah');
-form.addEventListener('click', ()=>{
-    modal_form.classList.toggle('hidden');
-    modal_form.classList.add('flex');
-    modal_form.classList.add(layer_h);
-    layer.classList.toggle('hidden');
-    layer.classList.add(layer_h);
-    header.classList.add(bg);
-    container.classList.add(bg);
-});
-
-// detail
-const id_details = document.querySelectorAll('#detail span')
+// form detail
+const details = document.querySelectorAll('#detail span')
 const modal_detail = document.getElementById('modal-detail');
-const details = document.querySelectorAll('.ri-search-line');
-
-const cek_detail = function(e){
-    e.addEventListener('click', ()=>{
-        modal_detail.classList.toggle('hidden');
-        layer.classList.toggle('hidden');
-        layer.classList.add(layer_h);
-        modal_detail.classList.add('flex');
-        modal_detail.classList.add(layer_h);
-        header.classList.add(bg);
-        container.classList.add(bg);
-
-        e.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
+const tabel = document.getElementById('tabel');
+tabel.addEventListener('click', function(e){
+    if(e.target.className == "ri-search-line text-white"){
+        display();
+        modal(modal_detail);
+        e.target.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
         const td = document.querySelectorAll('#cek td');
         for(let i = 1; i <= 5; i++){
-            id_details[i-1].innerText = td[i].innerText;
+            details[i].innerText = td[i].innerText;
         }
-    })
-}
-details.forEach(cek_detail);
+    } 
+})
+//-------------------------
 
-//hapus
+//---------------hapus
 const modal_hapus = document.getElementById('modal-hapus');
 const hapus = document.querySelectorAll('.ri-delete-bin-line');
 const cek_hapus = function(e){
     e.addEventListener('click', ()=>{
-        modal_hapus.classList.toggle('hidden');
-        layer.classList.toggle('hidden');
-        layer.classList.add(layer_h);
-        modal_hapus.classList.add('flex');
-        modal_hapus.classList.add(layer_h);
-        header.classList.add(bg);
-        container.classList.add(bg);
+        display();
+        modal(modal_hapus);
     })
 }
 hapus.forEach(cek_hapus);
 
-// edit
+//----------------edit
 const modal_edit = document.getElementById('modal-edit');
 const edits = document.querySelectorAll('.ri-file-edit-line');
 const cek_edit = function(e){
     e.addEventListener('click', ()=>{
-        modal_edit.classList.toggle('hidden');
-        modal_edit.classList.add('flex');
-        modal_edit.classList.add(layer_h);
-        layer.classList.toggle('hidden');
-        layer.classList.add(layer_h);
-        header.classList.add(bg);
-        container.classList.add(bg);
+        display();
+        modal(modal_edit);
     })
 }
 edits.forEach(cek_edit);
