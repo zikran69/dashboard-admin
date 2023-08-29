7ce5de3f7a986ee0df13d22858f3266cca9b0b5a
bahe <bahebdg@gmail.com>
2023-08-29 23:39:05 +0700

    change selector from icon to button

--- src/js/kategori-kamar.js
+++ src/js/kategori-kamar.js
@@ -46,7 +46,7 @@ for(const span of detail_spans){
     span.classList.add('text-lg');
 }
 // 
-const details = document.querySelectorAll('#detail span');
+
 const modal_detail = document.getElementById('modal-detail');
 const modal_hapus = document.getElementById('modal-hapus');
 const modal_edit = document.getElementById('modal-edit');
@@ -56,10 +56,11 @@ const text_areas = document.querySelectorAll('#modal-edit textarea');
 const setatus = document.querySelector('#modal-edit select');
 const simpan = document.querySelectorAll('#modal-edit button')[1];
 
-const tabel = document.getElementById('tabel');
-tabel.addEventListener('click', function(e){
-    // detail
-    if(e.target.className == "ri-search-line text-white"){
+// DETAIL   
+const details = document.querySelectorAll('#detail span');
+const details_selector = document.querySelectorAll('.detail');
+const cek_detail = (detail) =>{
+    detail.addEventListener('click', (e)=>{
         display();
         modal(modal_detail);
         e.target.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
@@ -67,14 +68,24 @@ tabel.addEventListener('click', function(e){
         for(let i = 1; i <= 5; i++){
             details[i].innerText = td[i].innerText;
         }
-    } 
-    // hapus
-    else if(e.target.className == "ri-delete-bin-line text-white"){
+    })
+}
+details_selector.forEach(cek_detail);
+
+// HAPUS
+const hapus_selector = document.querySelectorAll('.hapus');
+const cek_hapus = (hapus)=>{
+    hapus.addEventListener('click', (e)=>{
         display();
         modal(modal_hapus);
-    }
-    // edit
-    else if(e.target.className == "ri-file-edit-line text-white"){
+    })
+}
+hapus_selector.forEach(cek_hapus);
+
+// EDIT
+const edits_selector = document.querySelectorAll('.edit');
+const cek_edit = (edit)=>{
+    edit.addEventListener('click', (e)=>{
         display();
         modal(modal_edit);
 
@@ -90,17 +101,56 @@ tabel.addEventListener('click', function(e){
         text_areas[0].value = tds[3].innerText;
         text_areas[1].value = tds[4].innerText;
         setatus.value = tds[5].innerText;
+    })
+}
+edits_selector.forEach(cek_edit);
 
-        // simpan.addEventListener('click', ()=>{  
-        //     tds[1].innerText = inputan[0].value;
-        //     tds[2].innerText = inputan[1].value;
-        //     tds[3].innerText = text_areas[0].value;
-        //     tds[4].innerText = text_areas[1].value;
-        //     tds[5].innerText = setatus.value;
+
+// const tabel = document.getElementById('tabel');
+// tabel.addEventListener('click', function(e){
+//     // detail
+//     if(e.target.className == "ri-search-line text-white"){
+        // display();
+        // modal(modal_detail);
+        // e.target.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
+        // const td = document.querySelectorAll('#cek td');
+        // for(let i = 1; i <= 5; i++){
+        //     details[i].innerText = td[i].innerText;
+        // }
+//     } 
+//     // hapus
+//     else if(e.target.className == "ri-delete-bin-line text-white"){
+        // display();
+        // modal(modal_hapus);
+//     }
+//     // edit
+//     else if(e.target.className == "ri-file-edit-line text-white"){
+        // display();
+        // modal(modal_edit);
+
+        // const inputan = document.querySelectorAll('#modal-edit input');
+        // const text_areas = document.querySelectorAll('#modal-edit textarea');
+        // const setatus = document.querySelector('#modal-edit select');
+        // const simpan = document.querySelectorAll('#modal-edit button')[1];
+        // e.target.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
+        // const tds = document.querySelectorAll('#cek td');
+
+        // inputan[0].value = tds[1].innerText;
+        // inputan[1].value = tds[2].innerText;
+        // text_areas[0].value = tds[3].innerText;
+        // text_areas[1].value = tds[4].innerText;
+        // setatus.value = tds[5].innerText;
+
+//         // simpan.addEventListener('click', ()=>{  
+//         //     tds[1].innerText = inputan[0].value;
+//         //     tds[2].innerText = inputan[1].value;
+//         //     tds[3].innerText = text_areas[0].value;
+//         //     tds[4].innerText = text_areas[1].value;
+//         //     tds[5].innerText = setatus.value;
             
-        //     display();
-        //     modal(modal_edit);
-        // })      
-    }
-})
+//         //     display();
+//         //     modal(modal_edit);
+//         // })      
+//     }
+// })
 //-------------------------
--- src/js/lantai-kamar.js
+++ src/js/lantai-kamar.js
@@ -56,10 +56,9 @@ const text_areas = document.querySelectorAll('#modal-edit textarea');
 const setatus = document.querySelector('#modal-edit select');
 const simpan = document.querySelectorAll('#modal-edit button')[1];
 
-const tabel = document.getElementById('tabel');
-tabel.addEventListener('click', function(e){
-    // detail
-    if(e.target.className == "ri-search-line text-white"){
+const details_selector = document.querySelectorAll('.detail');
+const cek_detail = (detail) =>{
+    detail.addEventListener('click', (e)=>{
         display();
         modal(modal_detail);
         e.target.parentElement.parentElement.parentElement.parentElement.setAttribute('id', 'cek');
@@ -67,14 +66,24 @@ tabel.addEventListener('click', function(e){
         for(let i = 1; i <= 3; i++){
             details[i].innerText = td[i].innerText;
         }
-    } 
-    // hapus
-    else if(e.target.className == "ri-delete-bin-line text-white"){
+    })
+}
+details_selector.forEach(cek_detail);
+
+// HAPUS
+const hapus_selector = document.querySelectorAll('.hapus');
+const cek_hapus = (hapus)=>{
+    hapus.addEventListener('click', (e)=>{
         display();
         modal(modal_hapus);
-    }
-    // edit
-    else if(e.target.className == "ri-file-edit-line text-white"){
+    })
+}
+hapus_selector.forEach(cek_hapus);
+
+// EDIT
+const edits_selector = document.querySelectorAll('.edit');
+const cek_edit = (edit)=>{
+    edit.addEventListener('click', (e)=>{
         display();
         modal(modal_edit);
 
@@ -88,17 +97,6 @@ tabel.addEventListener('click', function(e){
         inputan[0].value = tds[1].innerText;
         text_areas[0].value = tds[2].innerText;
         setatus.value = tds[3].innerText;
-
-        // simpan.addEventListener('click', ()=>{  
-        //     tds[1].innerText = inputan[0].value;
-        //     tds[2].innerText = inputan[1].value;
-        //     tds[3].innerText = text_areas[0].value;
-        //     tds[4].innerText = text_areas[1].value;
-        //     tds[5].innerText = setatus.value;
-            
-        //     display();
-        //     modal(modal_edit);
-        // })      
-    }
-})
-//-------------------------
+    })
+}
+edits_selector.forEach(cek_edit);
